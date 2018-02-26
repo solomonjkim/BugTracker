fec681ef5c3528c1281643117903b58219e467c5
ksolomon <ksolomon@andrews.edu>
2018-02-26 15:49:31 -0500

    renamed BugTrackerActivity to BugDetailsActivity

--- app/src/main/java/edu/andrews/cptr252/ksolomon/bugtracker/BugDetailsActivity.java
+++ app/src/main/java/edu/andrews/cptr252/ksolomon/bugtracker/BugDetailsActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class BugTrackerActivity extends SingleFragmentActivity {
+public class BugDetailsActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment(){
