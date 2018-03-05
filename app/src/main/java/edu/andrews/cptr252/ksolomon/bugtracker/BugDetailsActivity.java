3f948bd08a9181768f9ac6db25f5efdba848fa4d
ksolomon <ksolomon@andrews.edu>
2018-03-05 16:12:05 -0500

    Can swipe to edit next/previous bug

--- app/src/main/java/edu/andrews/cptr252/ksolomon/bugtracker/BugDetailsActivity.java
+++ app/src/main/java/edu/andrews/cptr252/ksolomon/bugtracker/BugDetailsActivity.java
@@ -9,19 +9,77 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import java.util.UUID;
+import java.util.ArrayList;
 
-public class BugDetailsActivity extends SingleFragmentActivity {
+public class BugDetailsActivity extends FragmentActivity {
+
+    private ViewPager mViewPager;
+
+    private ArrayList<Bug> mBugs;
 
     @Override
-    protected Fragment createFragment(){
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
 
-        UUID bugId = (UUID)getIntent().getSerializableExtra(BugDetailsFragment.EXTRA_BUG_ID);
+        mViewPager = new ViewPager(this);
 
-        return BugDetailsFragment.newInstance(bugId);
+        mViewPager.setId(R.id.viewPager);
+
+        setContentView(mViewPager);
+
+        mBugs = BugList.getInstance(this).getBugs();
+
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                Bug bug = mBugs.get(position);
+                return BugDetailsFragment.newInstance(bug.getID());
+            }
+
+            @Override
+            public int getCount() {
+                return mBugs.size();
+            }
+        });
+
+        UUID bugId = (UUID) getIntent().getSerializableExtra(BugDetailsFragment.EXTRA_BUG_ID);
+
+
+        for (int i = 0; i < mBugs.size(); i++){
+            if(mBugs.get(i).getID().equals(bugId)){
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+
+        }
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener(){
+
+            @Override
+            public void onPageScrolled(int i, float v, int i2){
+
+            }
+
+            @Override
+            public void onPageSelected(int position){
+                Bug bug = mBugs.get(position);
+                if(bug.getTitle()!= null){
+                    setTitle(bug.getTitle());
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i){
+
+            }
+        });
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
